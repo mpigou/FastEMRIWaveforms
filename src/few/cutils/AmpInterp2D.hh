b99bb38a54c404c08bdf9a3dd23eaee2124577c3
Maxime PIGOU <maxime.pigou@l2it.in2p3.fr>
2025-03-07 12:30:00 +0000

    lint: apply cmake-format linter

--- src/few/cutils/AmpInterp2D.hh
+++ src/few/cutils/AmpInterp2D.hh
@@ -3,8 +3,8 @@
 
 #include "global.h"
 
-void interp2D_wrap(double* z, const double* tx, int nx, const double* ty, int ny, double* c,
-             int kx, int ky, const double* x, int mx,
-             const double* y, int my, int num_indiv_c, int len_indiv_c);
+void interp2D_wrap(double* z, const double* tx, int nx, const double* ty,
+                   int ny, double* c, int kx, int ky, const double* x, int mx,
+                   const double* y, int my, int num_indiv_c, int len_indiv_c);
 
-#endif // __AMP_INTERP_2D_HH__
+#endif  // __AMP_INTERP_2D_HH__
--- src/few/cutils/global.h
+++ src/few/cutils/global.h
@@ -1,33 +1,34 @@
 #ifndef _GLOBAL_HEADER_
 #define _GLOBAL_HEADER_
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 #include <complex>
+
 #include "cuda_complex.hpp"
 
 #if defined(_MSC_VER)
-    #define LAPACK_COMPLEX_CUSTOM
-    #define lapack_complex_float _Fcomplex
-    #define lapack_complex_double _Dcomplex
-    #define _USE_MATH_DEFINES
-    #define FEW_INLINE __inline
+#define LAPACK_COMPLEX_CUSTOM
+#define lapack_complex_float _Fcomplex
+#define lapack_complex_double _Dcomplex
+#define _USE_MATH_DEFINES
+#define FEW_INLINE __inline
 #else
-    #define FEW_INLINE __inline__
+#define FEW_INLINE __inline__
 #endif
 
 #include <cmath>
 
 // Definitions needed for Mathematicas CForm output
-#define Power(x, y)     (pow((double)(x), (double)(y)))
-#define Sqrt(x)         (sqrt((double)(x)))
-
+#define Power(x, y) (pow((double)(x), (double)(y)))
+#define Sqrt(x) (sqrt((double)(x)))
 
 // Constants below from lisaconstants -- all in units of seconds
 #define YRSID_SI 31558149.763545595
 #define MTSUN_SI 4.9254909491978065e-06
 
-#define  GPCINSEC 1.02927125054339e+17
+#define GPCINSEC 1.02927125054339e+17
 #define AUsec 499.00478383615643
 
 typedef double fod;
@@ -46,19 +47,22 @@ typedef gcmplx::complex<double> cmplx;
 #endif
 
 #ifdef __CUDACC__
-#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
-{
-   if (code != cudaSuccess)
-   {
-      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
-      if (abort) exit(code);
-   }
+#define gpuErrchk(ans)                    \
+  {                                       \
+    gpuAssert((ans), __FILE__, __LINE__); \
+  }
+inline void gpuAssert(cudaError_t code, const char *file, int line,
+                      bool abort = true) {
+  if (code != cudaSuccess) {
+    fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file,
+            line);
+    if (abort) exit(code);
+  }
 }
 
 #endif
 
-typedef struct tagInterpContainer{
+typedef struct tagInterpContainer {
   fod *y;
   fod *c1;
   fod *c2;
@@ -68,31 +72,30 @@ typedef struct tagInterpContainer{
 } InterpContainer;
 
 typedef struct tagFilterContainer {
-
-    int *d_mode_keep_inds;
-    int *d_filter_modes_buffer;
-    fod *working_modes_all;
-    int *ind_working_modes_all;
-    int *d_num_modes_kept;
-    int num_modes_kept;
-    fod tol;
+  int *d_mode_keep_inds;
+  int *d_filter_modes_buffer;
+  fod *working_modes_all;
+  int *ind_working_modes_all;
+  int *d_num_modes_kept;
+  int num_modes_kept;
+  fod tol;
 
 } FilterContainer;
 
 typedef struct tagModeReImContainer {
-    fod *re_y;
-    fod *re_c1;
-    fod *re_c2;
-    fod *re_c3;
+  fod *re_y;
+  fod *re_c1;
+  fod *re_c2;
+  fod *re_c3;
 
-    fod *im_y;
-    fod *im_c1;
-    fod *im_c2;
-    fod *im_c3;
+  fod *im_y;
+  fod *im_c1;
+  fod *im_c2;
+  fod *im_c3;
 
 } ModeReImContainer;
 
-
-typedef void (*fptr)(double *, double *, double *, double *, double *, double *, double, double, double, double, double);
+typedef void (*fptr)(double *, double *, double *, double *, double *, double *,
+                     double, double, double, double, double);
 
 #endif  // _GLOBAL_HEADER_
