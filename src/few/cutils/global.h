9c73d03c24123f7183852e790b0811eb603026c2
Maxime PIGOU <maxime.pigou@l2it.in2p3.fr>
2025-04-25 13:22:27 +0000

    chore: apply clang-format on compiled sources

--- src/few/cutils/global.h
+++ src/few/cutils/global.h
@@ -2,28 +2,27 @@
 #define _GLOBAL_HEADER_
 
 #if defined(_MSC_VER)
-    #define _USE_MATH_DEFINES
-    #define FEW_INLINE __inline
+#define _USE_MATH_DEFINES
+#define FEW_INLINE __inline
 #else
-    #define FEW_INLINE __inline__
+#define FEW_INLINE __inline__
 #endif
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <complex>
 #include "cuda_complex.hpp"
 #include <cmath>
+#include <complex>
+#include <stdio.h>
+#include <stdlib.h>
 
 // Definitions needed for Mathematicas CForm output
-#define Power(x, y)     (pow((double)(x), (double)(y)))
-#define Sqrt(x)         (sqrt((double)(x)))
-
+#define Power(x, y) (pow((double)(x), (double)(y)))
+#define Sqrt(x) (sqrt((double)(x)))
 
 // Constants below from lisaconstants -- all in units of seconds
 #define YRSID_SI 31558149.763545595
 #define MTSUN_SI 4.9254909491978065e-06
 
-#define  GPCINSEC 1.02927125054339e+17
+#define GPCINSEC 1.02927125054339e+17
 #define AUsec 499.00478383615643
 
 typedef double fod;
@@ -42,19 +41,23 @@ typedef gcmplx::complex<double> cmplx;
 #endif
 
 #ifdef __CUDACC__
-#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
-{
-   if (code != cudaSuccess)
-   {
-      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
-      if (abort) exit(code);
-   }
+#define gpuErrchk(ans)                                                         \
+  {                                                                            \
+    gpuAssert((ans), __FILE__, __LINE__);                                      \
+  }
+inline void gpuAssert(cudaError_t code, const char *file, int line,
+                      bool abort = true) {
+  if (code != cudaSuccess) {
+    fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file,
+            line);
+    if (abort)
+      exit(code);
+  }
 }
 
 #endif
 
-typedef struct tagInterpContainer{
+typedef struct tagInterpContainer {
   fod *y;
   fod *c1;
   fod *c2;
@@ -65,30 +68,30 @@ typedef struct tagInterpContainer{
 
 typedef struct tagFilterContainer {
 
-    int *d_mode_keep_inds;
-    int *d_filter_modes_buffer;
-    fod *working_modes_all;
-    int *ind_working_modes_all;
-    int *d_num_modes_kept;
-    int num_modes_kept;
-    fod tol;
+  int *d_mode_keep_inds;
+  int *d_filter_modes_buffer;
+  fod *working_modes_all;
+  int *ind_working_modes_all;
+  int *d_num_modes_kept;
+  int num_modes_kept;
+  fod tol;
 
 } FilterContainer;
 
 typedef struct tagModeReImContainer {
-    fod *re_y;
-    fod *re_c1;
-    fod *re_c2;
-    fod *re_c3;
+  fod *re_y;
+  fod *re_c1;
+  fod *re_c2;
+  fod *re_c3;
 
-    fod *im_y;
-    fod *im_c1;
-    fod *im_c2;
-    fod *im_c3;
+  fod *im_y;
+  fod *im_c1;
+  fod *im_c2;
+  fod *im_c3;
 
 } ModeReImContainer;
 
+typedef void (*fptr)(double *, double *, double *, double *, double *, double *,
+                     double, double, double, double, double);
 
-typedef void (*fptr)(double *, double *, double *, double *, double *, double *, double, double, double, double, double);
-
-#endif  // _GLOBAL_HEADER_
+#endif // _GLOBAL_HEADER_
