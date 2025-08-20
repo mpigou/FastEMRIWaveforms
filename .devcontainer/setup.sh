3f2c64115b172b497eaf4571c23c9449c32772e6
Maxime Pigou <maxime.pigou@l2it.in2p3.fr>
2025-08-20 09:02:28 +0000

    ci: fix use of uv 1/? [ci:build-wheels] [ci:build-pages]

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,26 +3,16 @@
 # Stop on error
 set -e
 
-# ==============
-# Install a venv
-# ==============
-python3 -m venv ~/.local/few-venv
-source ~/.local/few-venv/bin/activate
-
 # ===============
 # Fix permissions
 # ===============
 sudo chown -R few:few /workspaces
 
-# ==================
-# Install pre-commit
-# ==================
-pip install --no-cache-dir pre-commit
-
 # ====================
 # Configure pre-commit
 # ====================
-pre-commit install \
+uv tool install pre-commit --with pre-commit-uv
+uv tool run pre-commit install \
   --hook-type commit-msg \
   --hook-type pre-commit \
   --hook-type pre-push
